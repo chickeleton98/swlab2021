d37688581793f63880373f8f5a69f261bcc56b11
Han Minjae <chickeleton98@gmail.com>
2021-04-15 15:31:24 +0900

    initial commit

---
+++ src/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void foo(void)
+{
+	printf("hello foo\n");
+}
---
+++ src/goo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "funcs.h"
+
+void goo(void)
+{
+	printf("goo\n");
+}
+
